e64cd831483fcd2e45b2eaa6df37453af62a8a13
vanillaaicecream <vattiruchi08.2007@gmail.com>
2026-06-29 11:04:30 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
-print("hello world")
-a=10
-b=20
-c=a+b
-print("The sum of a and b is:", c)
-print("flask")
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr):
+        arr.sort()
+        arr[0] = 1
+        
+        for i in range(1, len(arr)):
+            arr[i] = min(arr[i], arr[i-1] + 1)
+        
+        return arr[-1]

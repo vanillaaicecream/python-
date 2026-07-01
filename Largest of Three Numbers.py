24b60165b907e9b9904a62e8c5c9c3579ef50564
vanillaaicecream <vattiruchi08.2007@gmail.com>
2026-07-01 20:32:26 +0530

    Create Largest of Three Numbers.py

---
+++ Largest of Three Numbers.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = max(a, b, c)
+print("Largest number is:", largest)
